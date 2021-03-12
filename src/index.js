3e6ac6152ca9497cbac0d86358036ae462a1f538
Strelnikov22 <vadim.strelnikov.1999@gmail.com>
2021-03-12 22:39:26 +0100

    added animation and opened Modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import ModalWindow from './modules/modalWindow.js';
 import Slider from './modules/slider.js';
 
+const modal = {
+    overlay:'.modal-overlay',
+    modal:'.modal-callback',
+    closeBtn:'.modal-close',
+    openBtn:'.header .callback-btn',
+    animation:{
+        overlayOpen:'animate__fadeIn',
+        overlayClose:'animate__fadeOut',
+        typeOpen:'animate__zoomIn',
+        typeClose:'animate__zoomOut',
+    }
+};
 
+const modalWin = new ModalWindow(modal);
+
+modalWin.init();
 
 
 
--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -1,7 +1,92 @@
 'use strict';
 
 class ModalWindow {
-   
+   constructor({
+       overlay,
+       modal,
+       closeBtn,
+       openBtn,
+       animation = {
+            overlayOpen:'animate__fadeIn',
+            overlayClose:'animate__fadeOut',
+            typeOpen: 'animate__fadeIn',
+            typeClose: 'animate__fadeOut'
+        },
+   }){
+    this.overlay = document.querySelector(overlay),
+    this.modal = document.querySelector(modal),
+    this.closeBtn = document.querySelector(closeBtn),
+    this.openBtn = document.querySelector(openBtn),
+    this.animation = animation;
+   };
+
+   //----Подключаем анимацию-----
+   linkedAnimate(){
+       const link = document.createElement('link');
+       link.setAttribute('rel', 'stylesheet');
+       link.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
+       document.head.append(link);
+   };
+
+   modalEvent(){
+       this.openBtn.addEventListener('click', this.openModal.bind(this));
+       this.closeBtn.addEventListener('click', this.closeModal.bind(this));
+       this.overlay.addEventListener('click', this.closeModal.bind(this));
+   };
+
+   //--------Открытие модального окна--------
+   openModal(event){
+       event.preventDefault();
+       
+       this.overlay.style.display = 'block'
+       this.overlay.classList.remove(this.animation.overlayClose);
+       this.overlay.classList.add('animate__animated', this.animation.overlayOpen);
+       setTimeout(this.animateModal.bind(this), 1000);
+   };
+
+   animateModal(){
+        this.modal.style.display = 'block';
+        this.modal.classList.remove(this.animation.typeClose);
+        this.modal.classList.add('animate__animated', this.animation.typeOpen);
+   };
+
+   //--------Закрытие модального окна-------
+   closeModal(){
+        setTimeout(this.closeOverlay.bind(this), 500);
+       
+       this.modal.classList.remove(this.animation.typeOpen);
+       this.modal.classList.add(this.animation.typeClose);
+       setTimeout(this.setMod.bind(this), 300);
+   }
+
+   closeOverlay(){
+       this.overlay.classList.remove(this.animation.overlayOpen);
+       this.overlay.classList.add(this.animation.overlayClose);
+       setTimeout(this.setOver.bind(this), 300);
+   }
+
+   setOver(){
+        this.overlay.style.display = 'none';
+        this.clearStyle();
+   }
+
+   setMod(){
+        this.modal.style.display = 'none';
+   }
+
+   //----чистим классы и стили после использования----
+   clearStyle(){
+       this.modal.classList.remove('animate__animated', this.animation.typeOpen, this.animation.typeClose);
+       this.overlay.classList.remove('animate__animated', this.animation.overlayOpen, this.animation.overlayClose);
+       this.modal.removeAttribute('style');
+       this.overlay.removeAttribute('style');
+   }
+
+   init(){
+       this.linkedAnimate();
+       this.modalEvent();
+       console.log(this)
+   };
 } 
 
 export default ModalWindow;
